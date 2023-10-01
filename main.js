127f8b9c3a2e5b389d0567a115e48a8f4d42c794
Marcos02091267384 <maalfepi@gmail.com>
2023-10-01 18:32:53 -0400

    FinDelProyecto

--- main.js
+++ main.js
@@ -6,3 +6,4 @@ funciones.actualizar(registro, './Registro.json');
 funciones.editar('./Registro.json', 0, new AlumnoCurso("Jose", 40, 3));
 funciones.eliminarIndice(0,'./Registro.json');
 console.log("Fin de la ejecucion");
+//Como no me hablan nada de validar datos asumo que es imnecesario.
